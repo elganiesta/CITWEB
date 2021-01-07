cb5b133b1e0697e3da52d09139382aa243e3e639
elganiesta <54721994+elganiesta@users.noreply.github.com>
2021-01-07 21:19:27 +0100

    Javascript basics

---
+++ script.js
@@ -0,0 +1,112 @@
+// let name = "bob";
+// var obj = Object();
+// var obj2 = {
+//     "name": 1
+// };
+
+// obj.name = 1;
+// var flag ;
+// if(flag == true) {
+//     console.log(flag);
+// } else {
+//     console.log(flag);
+// }
+
+// flag == true ? 
+// console.log("yes") : 
+// console.log("No !");
+
+// console.log(flag ?? "Noo..");
+
+// console.log(obj);
+
+// var name = "luis";
+// switch(name) {
+//     case "luis" :
+//         console.log("yes");
+//         break;
+    
+// }
+
+// var flag = false;
+// var userName = "luis";
+
+// if(!flag) {
+//     console.log('bienvenue ! ');
+// } else {
+//     console.log('sirf7alk')
+// }
+
+// var somme = function nameFunction(a,b,c) {
+//     return a + b +c ;
+// }
+
+// var somme = (a,b) => {
+//     return a+b;
+// }
+
+// var somme = (a,b) => a + b; 
+// var multiple2 = a => a * 2; 
+
+// var sommeABC = somme(1,5);
+// var multipleA = multiple2(5);
+
+// console.log(multipleA);
+
+// var list = Array('name', 2);
+// var list2 = ['name', 2];
+
+// var obj = Object();
+// obj.name = "name";
+
+// var obj = {
+//     name : "hhhhhh"
+// };
+
+// console.log(obj['name']);
+// console.log(obj.name);
+
+
+// for(var i=0;i<10;i++) {
+//     console.log(i);
+// }
+
+// var i = 0;
+// while(i<10) {
+//     console.log(i);
+//     i++;
+// }
+
+// var list = [1,2,3,4,5,6,7,8,9,10];
+
+// for(var number of list) {
+//     console.log(number);
+// }
+
+// list.forEach(item => console.log(item))
+
+// try {
+//     var flag = null;
+//     flag.forEach(element => {
+//         console.log("hey")
+//     });
+// } catch (error) {
+//     alert('alert')
+//     var message = prompt("Sorry.. report this problem !")
+//     console.log(message)
+// }
+
+// var message = "String !";
+
+
+// if(typeof message == "string") {
+//     console.log("it's string");
+// } else {
+//     console.log("it's not");
+// }
+
+
+// ? madirch frask 
+// var clicked = () => {
+//     document.getElementById('paragraph').innerHTML = "yes you clicked me"
+// }
