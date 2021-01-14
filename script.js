22b8f1a3152ca7f423c1b1720d7c2cbde0c16a95
elganiesta <54721994+elganiesta@users.noreply.github.com>
2021-01-14 21:28:06 +0100

    JS dom

--- script.js
+++ script.js
@@ -1,112 +1,102 @@
-// let name = "bob";
-// var obj = Object();
-// var obj2 = {
-//     "name": 1
-// };
+// ? get element by id
 
-// obj.name = 1;
-// var flag ;
-// if(flag == true) {
-//     console.log(flag);
-// } else {
-//     console.log(flag);
+// const h1 = document.getElementById('heading');
+// const title = document.getElementsByTagName('h1');
+// const titleDiv = document.getElementsByClassName('title');
+
+// console.log(title[0]);
+// console.log(h1);
+// console.log(titleDiv[0])
+
+// ? query selector
+
+// const h1 = document.querySelector('#heading');
+// const title = document.querySelector('h1');
+// const titleDiv = document.querySelector('.title');
+
+// const ids = document.querySelectorAll('#heading')
+// console.log(ids);
+
+// var heading = document.getElementById('heading');
+// heading.innerHTML = 'Heading 2';
+// heading.className = 'class2';
+// heading.style.color = 'black';
+// heading.style.textDecoration = 'underline';
+// heading.setAttribute('class', 'class2');
+
+// ? create elements
+
+// var heading = document.createElement('h1');
+// heading.className = 'class2';
+// heading.id = 'heading2';
+// heading.innerHTML = 'New heading';
+
+// var div = document.createElement('div');
+// div.className = 'title';
+
+// div.appendChild(heading);
+
+// var collection = document.querySelector('.collections');
+// collection.appendChild(div);
+// var title1 = document.getElementById('title1');
+// collection.replaceChild(div, title1)
+
+// var title1 = document.getElementById('title1');
+// var heading1 = document.getElementById('heading');
+// title1.removeChild(heading1);
+
+var collection = document.querySelector('#div');
+collection.innerHTML = `
+    <h1 id="h1">Heading2</h1>
+`
+
+// ? event listenners
+// var heading = document.getElementById('heading');
+
+// heading.onclick = (event) => {
+//     heading.innerHTML = 'you clicked me';
+//     console.log(event.target.className);
+// }
+// var clicked = 0;
+// const changeContext = () => {
+//     heading.innerHTML = 'you clicked me ' + clicked;
+    // clicked++;
+    // if (clicked == 5) {
+    //     heading.removeEventListener('click', changeContext);
+    // }
 // }
 
-// flag == true ? 
-// console.log("yes") : 
-// console.log("No !");
+// heading.addEventListener('click', changeContext);
 
-// console.log(flag ?? "Noo..");
+// ? select all 
 
-// console.log(obj);
+// console.log(document.anchors);
 
-// var name = "luis";
-// switch(name) {
-//     case "luis" :
-//         console.log("yes");
-//         break;
-    
-// }
-
-// var flag = false;
-// var userName = "luis";
-
-// if(!flag) {
-//     console.log('bienvenue ! ');
-// } else {
-//     console.log('sirf7alk')
-// }
-
-// var somme = function nameFunction(a,b,c) {
-//     return a + b +c ;
-// }
-
-// var somme = (a,b) => {
-//     return a+b;
-// }
-
-// var somme = (a,b) => a + b; 
-// var multiple2 = a => a * 2; 
-
-// var sommeABC = somme(1,5);
-// var multipleA = multiple2(5);
-
-// console.log(multipleA);
-
-// var list = Array('name', 2);
-// var list2 = ['name', 2];
-
-// var obj = Object();
-// obj.name = "name";
-
-// var obj = {
-//     name : "hhhhhh"
-// };
-
-// console.log(obj['name']);
-// console.log(obj.name);
+// ? forms
 
 
-// for(var i=0;i<10;i++) {
-//     console.log(i);
-// }
+// var submit = document.getElementById('submit');
+// var username = document.getElementById('username');
 
-// var i = 0;
-// while(i<10) {
-//     console.log(i);
-//     i++;
-// }
+// var form = document.getElementById('form');
 
-// var list = [1,2,3,4,5,6,7,8,9,10];
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     console.log(form.username.value);
+//     console.log(form.password.value);
+//     // form.reset();
+//     form.username.value = '';
+//     form.password.value = '';
+// });
 
-// for(var number of list) {
-//     console.log(number);
-// }
+// var username = document.querySelector("input[name='username']");
+// console.log(username);
 
-// list.forEach(item => console.log(item))
+// var h1 = document.getElementById('h1');
+// console.log(h1.parentNode)
+// console.log(h1.nextSibling.nextSibling.nodeValue)
 
-// try {
-//     var flag = null;
-//     flag.forEach(element => {
-//         console.log("hey")
-//     });
-// } catch (error) {
-//     alert('alert')
-//     var message = prompt("Sorry.. report this problem !")
-//     console.log(message)
-// }
-
-// var message = "String !";
+// var div = document.getElementById('div');
+// console.log(div.childNodes)
 
 
-// if(typeof message == "string") {
-//     console.log("it's string");
-// } else {
-//     console.log("it's not");
-// }
-
-
-// ? madirch frask 
-// var clicked = () => {
-//     document.getElementById('paragraph').innerHTML = "yes you clicked me"
-// }
